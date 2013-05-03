a32548674536c5a839e19a17c20d2142343e0653
tomerk <tomer.kaftan@quixey.com>
2013-05-02 20:03:02 -0700

    Making the code nicer

--- src/fuse/__init__.py
+++ src/fuse/__init__.py
@@ -5,28 +5,19 @@ _nextNum = 0
 class Node():
     def __init__(self):
         global _nextNum
-        self.__node_num__ = _nextNum
+        self.nodeNum = _nextNum
         NODE_GRAPH[_nextNum] = set()
         _nextNum += 1
 
-    def getNodeNum(self):
-        return self.__node_num__
-
-    def getInput(self):
-        return self
-
-    def getOutput(self):
-        return self
-
     def __rshift__(self, other):
-        num1 = self.getNodeNum()
-        num2 = other.getNodeNum()
+        num1 = self.nodeNum
+        num2 = other.nodeNum
 
         # It needs to be an undirected graph so add both directions
         NODE_GRAPH[num1].add(num2)
         NODE_GRAPH[num2].add(num1)
 
-        return IOComponentWrapper(self, other)
+        return IOComponentWrapper([self], [other])
 
 GROUND = Node()
 
@@ -37,68 +28,51 @@ class Component():
     def getOutput(self):
         return Bundle(self.output)
 
+    def bundleIfIterables(self, i, j):
+        try:
+            i = Bundle(i)
+        except TypeError:
+            pass
+        try:
+            j = Bundle(j)
+        except TypeError:
+            pass
+        return i, j
+
     def __rshift__(self, other):
-        if (isinstance(other, Node)):
+        if isinstance(other, Node):
             self.getOutput()[0] >> other
             return IOComponentWrapper(self.getInput(), [other])
         elif isinstance(other, Component):
             for i, j in zip(self.getOutput(), other.getInput()):
-                try:
-                    i = Bundle(i)
-                except TypeError:
-                    pass
-                try:
-                    j = Bundle(j)
-                except TypeError:
-                    pass
+                i, j = self.bundleIfIterables(i, j)
                 i >> j
             return IOComponentWrapper(self.getInput(), other.getOutput())
         else:
             for i, j in zip(self.getOutput(), other):
-                try:
-                    i = Bundle(i)
-                except TypeError:
-                    pass
-                try:
-                    j = Bundle(j)
-                except TypeError:
-                    pass
+                i, j = self.bundleIfIterables(i, j)
                 i >> j
             return IOComponentWrapper(self.getInput(), other)
 
     def __rrshift__(self, other):
-        if (isinstance(other, Node)):
+        if isinstance(other, Node):
             other >> self.getInput()[0]
             return IOComponentWrapper([other], self.getOutput())
         elif isinstance(other, Component):
             for i, j in zip(other.getOutput(), self.getInput()):
-                try:
-                    i = Bundle(i)
-                except TypeError:
-                    pass
-                try:
-                    j = Bundle(j)
-                except TypeError:
-                    pass
+                i, j = self.bundleIfIterables(i, j)
                 i >> j
             return IOComponentWrapper(other.getInput(), self.getOutput())
         else:
             for i, j in zip(other, self.getInput()):
-                try:
-                    i = Bundle(i)
-                except TypeError:
-                    pass
-                try:
-                    j = Bundle(j)
-                except TypeError:
-                    pass
+                i, j = self.bundleIfIterables(i, j)
                 i >> j
             return IOComponentWrapper(other, self.getOutput())
 
 class IOComponentWrapper(Component):
-    def __init__(self, input, output):
-        self.input = input
-        self.output = output
+    def __init__(self, inp, out):
+        self.input = inp
+        self.output = out
 
 class Bundle(Component, list):
     def getInput(self):
