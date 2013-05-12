f6ec2993afcec41f95f18dbc226e3523cfcab042
tomerk <tomer.kaftan@quixey.com>
2013-05-11 21:26:52 -0700

    All examples are essentially functional

--- src/analogToDigital.py
+++ src/analogToDigital.py
@@ -3,18 +3,36 @@
 from fuse.core import *
 from fuse.primitives import *
 
-On = Ground() >> DCVoltageSource(1)
-Off = Ground()
-
 class XOrGate(CustomComponent):
-    pass
+    def __init__(self):
+        CustomComponent.__init__(self, [Node(), Node()], [Node()], "XOR")
+
+    def build(self):
+        On = Ground() >> DCVoltageSource(5)
+        Off = Ground()
+
+        modelEither = SwitchModel(VT = 2.5) # Switch w/ voltage threshold of 2.5
+        modelNeither = SwitchModel(VT = -2.5) # Switch w/ voltage threshold of -2.5
+        [On] + self.inp >> Switch(modelEither) >> self.out
+        [On] + self.inp[::-1] >> Switch(modelEither) >> self.out
+        ([Off] + self.inp >> Switch(modelNeither)).out + self.inp[::-1] >> Switch(modelNeither) >> self.out
+
 
 class GreaterThan(CustomComponent):
-    pass
+    def __init__(self):
+        CustomComponent.__init__(self, [Node(), Node()], [Node()], "Greater")
+
+    def build(self):
+        On = Ground() >> DCVoltageSource(5)
+        Off = Ground()
+
+        model = SwitchModel()
+        [On] + self.inp >> Switch(model) >> self.out
+        [Off] + self.inp[::-1] >> Switch(model) >> self.out
+
 
 class ThermometerEncoder(CustomComponent):
     # Create a thermometer analog -> digital encoder in the range zero_val to max_val
-
     def __init__(self, bits):
         inp = [Node()]
         out = Bus(bits)
@@ -22,75 +40,49 @@ class ThermometerEncoder(CustomComponent):
         super().__init__(inp, out, "ThermometerEncode" + str(bits))
 
     def build(self):
-        resistance = 1000
-
-        cur_node = On
+        cur_node = Ground() >> DCVoltageSource(5)
         for i in range(self.bits):
-            comparator = OpAmp()
-            comparator.connect(Bundle([self.input, cur_node, high, low]))
-            self.output[i].connect(comparator)
-
-            resistor = Resistor(resistance)
-            resistor.connect(cur_node)
-            cur_node = resistor
+            self.inp + [cur_node] >> GreaterThan() >> self.out[self.bits - i - 1]
+            cur_node = cur_node >> Resistor(1000)
 
         cur_node >> Ground()
 
 
-class AnalogToDigital(Component):
+class AnalogToDigital(CustomComponent):
     # Create an analog to digital encoding component
-    def __init__(self, bits, max_voltage):
-        self.input = Node()
-        self.output = Bundle([Node() for x in range(bits)])
-
-        # thermometer encoding logic
-        thermometerComponent = ThermometerEncoder(2 ** bits - 1, max_voltage)
-        thermometerComponent.connect(self.input)
-
-        # Thermometer encoding -> efficient digital bit representation
-        prev_node = ground
-        for i in reversed(range(2 ** bits - 1)):
-            xor = XOrGate()
-            xor.connect(Bundle(prev_node, thermometerComponent.output[i]))
-
-            # Example of making use of python functions to intelligently act
-            binary_array = [int(x) for x in reversed(bin(i+1)[2:])]
-            for j, k in enumerate(binary_array):
-                if k:
-                    self.output[j].connect(Diode(xor))
-
-            prev_node = thermometerComponent.output[i]
-
-        #Make sure that we set stuff relative to ground
-        for i in range(bits):
-            resistor = Resistor(2000)
-            resistor.connect(self.output[i])
-            ground.connect(resistor)
-
-class LowPassFilter(CustomComponent):
-    def __init__(self, cutoff):
-        inp, out, self.cutoff = [Node()], [Node()], cutoff
-        super().__init__(inp, out, 'lpf' + str(cutoff))
+    def __init__(self, bits):
+        self.bits = bits
+        super().__init__([Node()], Bus(bits), "AToD" + str(bits))
 
     def build(self):
-        self.inp >> Resistor(360) >> self.out
-        Ground() >> Capacitor(1.0 / (6.28 * 360 * self.cutoff)) >> self.out
+        # thermometer encoding logic
+        thermometerComponent = ThermometerEncoder(2 ** self.bits - 1)
+        self.inp >> thermometerComponent
 
+        # Thermometer encoding -> efficient digital bit representation
+        prev_node = Ground()
+        mod = DiodeModel()
+        for i in reversed(range(2 ** self.bits - 1)):
+            xor = XOrGate()
+            [prev_node, thermometerComponent.out[i]] >> xor
 
-class ChainedFilters(AbstractComponent):
-    def __init__(self, filter_list):
-        super().__init__([Node()], [Node()])
-        inp = self.inp
-        for filter_component in filter_list:
-            inp = inp >> filter_component
-        inp >> self.out
+            # Example of making use of python functions to intelligently act
+            binary_array = [int(x) for x in reversed(bin(i + 1)[2:])]
+            for j, k in enumerate(binary_array):
+                if k:
+                    xor >> Diode(mod) >> self.out[j]
 
+            prev_node = thermometerComponent.out[i]
 
-filter_list = [LowPassFilter(x) for x in [450] * 10]
-Ground() >> DCVoltageSource(1) >> ChainedFilters(filter_list) >> Resistor("1k")
+desiredNum = 2
+bits = 3
+voltage = 5 * desiredNum / ( 2 ** bits - 1)
+print("Voltage is: " + str(voltage))
 
-spiceNetlist = CircuitEnv.compileSpiceNetlist('chainedLPFs')
+Ground() >> DCVoltageSource(voltage) >> AnalogToDigital(bits) >> [Resistor("1k") >> Ground() for _ in range(bits)]
+
+spiceNetlist = CircuitEnv.compileSpiceNetlist('AnalogToDigital')
 print(spiceNetlist)
-f = open('/Users/tomerk/Desktop/chainedLowPassFilters.cir', 'w')
+f = open('/Users/tomerk/Desktop/AnalogToDigital.cir', 'w')
 f.write(spiceNetlist)
 f.close()
